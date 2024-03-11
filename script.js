ffd055afd20228b85e8ad8d9511ad4c08434eff6
Eshanmalik <159206492+Eshanmalik@users.noreply.github.com>
2024-03-11 14:13:58 +1100

    start layting out game

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let myIntro = 15;
-let enterName = 15;
+let enterName = prompt ("Enter your name");
+loadDIV ();
 
-if (myIntro == enterName) {
-    alert(myIntro);
+function loadDiv () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>" 
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDiv").innerHtml + "<H2>"Guess a number between 1 & 2</H2>"
+  }document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDiv")
+
+function enterNumber () {
+    return
 }
+
+function enternumber
+
